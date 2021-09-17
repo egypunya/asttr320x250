730b22c28b2064efb49a593b4385d058ba4de2ec
egypunya <egypunya@gmail.com>
2021-09-17 17:26:19 +0700

    Create astera320x250.js

---
+++ astera320x250.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'f2f71f26d3fce40a6bcf3e7878aa4a74',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://egypunya.github.io/f2f71f26d3fce40a6bcf3e7878aa4a74/invoke.js"></scr' + 'ipt>');
+</script>
